8f9e719cf03c21dda8e715c475e97d89c65b45bd
HSU0608 <tnsdnr0608@naver.com>
2022-10-14 01:31:49 +0900

    타이틀
    타이틀 추가

---
+++ image/play_state.py
@@ -0,0 +1,36 @@
+import game_framework
+import logo_state
+from pico2d import *
+import title_state
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 0, 90
+        self.frame = 0
+        self.image = load_image('run_animation.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        self.x += 1
+
+    def draw(self):
+        self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(logo_state)
+    delay(0.01)
+
